997410ef24e4ed0507c694a343366936157092bf
Anirudha Donge <anirudha.donge@gmail.com>
2025-12-02 09:01:55 +0530

    Actions - part2

--- Playwright-Training/PageModel/BasePage.js
+++ Playwright-Training/PageModel/BasePage.js
@@ -5,7 +5,7 @@ export class BasePage {
     this.page = page;
   }
 
-  async goto(url){
+  async goto(url) {
     console.log(`Navigate to ${url}`);
     await this.page.goto(url);
   }
@@ -15,9 +15,10 @@ export class BasePage {
     return this.page.locator(locatorString);
   }
 
-  async clickOnElement(locatorString){
+  async clickOnElement(locatorString) {
     console.log(`Click on ${locatorString}`);
-    await this.getLocator(locatorString).click();
+    const locator = this.getLocator(locatorString);
+    await locator.click();
   }
 
   async fill(locatorString, textToType) {
@@ -30,5 +31,21 @@ export class BasePage {
     await this.getLocator(locatorString).pressSequentially(textToType, {
       delay: delayTime,
     });
+  }
+
+  async uploadFile(locator, filePath) {
+    const fileChooser = this.page.waitForEvent("filechooser");
+    await this.page.locator(locator).click();
+    const fileChooserEvent = await fileChooser;
+    await fileChooserEvent.setFiles(filePath);
+  }
+
+  async downloadFile(fileLocator,filePath) {
+    const download = this.page.waitForEvent("download");
+    await this.page.locator(fileLocator).click();
+    const downloadEvent = await download;
+    await downloadEvent.saveAs(
+      filePath + downloadEvent.suggestedFilename()
+    );
   }
 }
--- Playwright-Training/tests/Actions.spec.js
+++ Playwright-Training/tests/Actions.spec.js
@@ -8,52 +8,125 @@
  * Gathering Text from the UI
  * Section options from dropdown
  * Javascript Alerts
- * Hover 
+ * Hover
  * New Page
  * Hover
  * Basic Authencation
  * Drag and Drop
  */
 
-import {test, expect} from '@playwright/test'
-import { BasePage } from '../PageModel/BasePage';
+import { test, expect } from "@playwright/test";
+import { BasePage } from "../PageModel/BasePage";
 
+test("Click Test", async ({ page }) => {
+  let basePage = new BasePage(page);
+  await basePage.goto("https://the-internet.herokuapp.com/");
+  await basePage.clickOnElement('[href="/login"]');
+  await basePage.pressSequentially("#username", "tomsmith", 3000);
+  await basePage.fill("#password", "SuperSecretPassword!");
+  // #await page.goto("https://the-internet.herokuapp.com/");
+  // await page.locator('[href="/login"]').click({
+  //     button:'left',
+  //     clickCount:1,
+  //     delay:3000,
+  //     force:false,
+  //     timeout:5000
+  // });
+  // await page.locator("#username").pressSequentially('tomsmith',{delay: 3000});
+  // await page.locator("#password").fill('SuperSecretPassword!');
 
-test.only('Click Test',async({page})=>{
-    let basePage = new BasePage(page);
-    await basePage.goto("https://the-internet.herokuapp.com/");
-    await basePage.clickOnElement('[href="/login"]');
-    await basePage.pressSequentially("#username",'tomsmith',3000);
-    await basePage.fill("#password",'SuperSecretPassword!');
-    // #await page.goto("https://the-internet.herokuapp.com/");
-    // await page.locator('[href="/login"]').click({
-    //     button:'left',
-    //     clickCount:1,
-    //     delay:3000,
-    //     force:false,
-    //     timeout:5000
-    // });
-    // await page.locator("#username").pressSequentially('tomsmith',{delay: 3000});
-    // await page.locator("#password").fill('SuperSecretPassword!');
+  await page.waitForTimeout(5000);
+});
 
-    await page.waitForTimeout(5000);
-})
+test("Checkbox test demo", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator('[href="/checkboxes"]').click();
+  await page.waitForTimeout(5000);
+  await page.locator("#checkboxes input").nth(0).check();
+  await page.waitForTimeout(5000);
+  await page.locator("#checkboxes input").nth(0).uncheck();
+  await page.waitForTimeout(5000);
+});
 
-test('Checkbox test demo',async({page})=>{
-    await page.goto("https://the-internet.herokuapp.com/");
-    await page.locator('[href="/checkboxes"]').click();
-    await page.waitForTimeout(5000);
-    await page.locator("#checkboxes input").nth(0).check();
-    await page.waitForTimeout(5000);
-    await page.locator("#checkboxes input").nth(0).uncheck();
-    await page.waitForTimeout(5000);
-})
+test("Iframe and frames test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator('[href="/frames"]').click();
+  await page.locator('[href="/iframe"]').click();
+  const framelocator = page.frameLocator("#mce_0_ifr");
+  await expect(framelocator.locator("#tinymce p")).toHaveText(
+    "Your content goes here."
+  );
+  await page.waitForTimeout(5000);
+});
 
-test("Iframe and frames test",async({page})=>{
-    await page.goto("https://the-internet.herokuapp.com/");
-    await page.locator('[href="/frames"]').click();
-    await page.locator('[href="/iframe"]').click();
-    const framelocator = page.frameLocator("#mce_0_ifr");
-    await expect(framelocator.locator("#tinymce p")).toHaveText('Your content goes here.');
-    await page.waitForTimeout(5000);
-})
+test("Dropdown selection test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator('[href="/dropdown"]').click();
+  await page.waitForTimeout(3000);
+  //   await page.locator("select#dropdown").selectOption({ value: "1" });
+  //   await page.waitForTimeout(3000);
+  //   await page.locator("select#dropdown").selectOption({ label: "Option 2" });
+  //   await page.waitForTimeout(3000);
+  //   await page.locator("select#dropdown").selectOption({ index: 1 });
+  //   await page.waitForTimeout(3000);
+  await page.locator("select#dropdown").click();
+  await page.waitForTimeout(3000);
+  await page.locator("select#dropdown option").nth(1).click();
+  await page.waitForTimeout(3000);
+});
+
+test("Upload file Test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator('[href="/upload"]').click();
+  //   await page
+  //     .locator("#file-upload")
+  //     .setInputFiles(
+  //       "C:/Users/adonge2d/source/PlaywrightJsNovBatch/Playwright-Training/uploadFile/uploadFile.pdf"
+  //     );
+
+  //   const fileChooser = page.waitForEvent('filechooser');
+  //   await page.locator("#file-upload").click();
+  //   const fileChooserEvent = await fileChooser;
+  //   await fileChooserEvent.setFiles("C:/Users/adonge2d/source/PlaywrightJsNovBatch/Playwright-Training/uploadFile/uploadFile.pdf");
+  let basePage = new BasePage(page);
+  await basePage.uploadFile(
+    "#file-upload",
+    "C:/Users/adonge2d/source/PlaywrightJsNovBatch/Playwright-Training/uploadFile/uploadFile.pdf"
+  );
+  await page.waitForTimeout(10000);
+});
+
+test("Download File test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator('[href="/download"]').click();
+  //   const download = page.waitForEvent('download');
+  //   await page.locator('.example a:has-text("TextDoc.txt")').click();
+  //   const downloadEvent = await download;
+  //   await downloadEvent.saveAs('./DownloadFile/'+downloadEvent.suggestedFilename());
+  let basePage = new BasePage(page);
+  await basePage.downloadFile(
+    '.example a:has-text("manualtesting_material.docx")',
+    "./DownloadFile/"
+  );
+  await page.waitForTimeout(5000);
+});
+
+test("Hover test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator('[href="/hovers"]').click();
+  await page.waitForTimeout(2000);
+  await page.locator(".figure img").nth(0).hover();
+  await page.waitForTimeout(2000);
+  await page.locator(".figure img").nth(1).hover();
+  await page.waitForTimeout(2000);
+  await page.locator(".figure img").nth(2).hover();
+  await page.waitForTimeout(2000);
+});
+
+test.only("Drag and Drop", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator('[href="/drag_and_drop"]').click();
+  await page.waitForTimeout(2000);
+  await page.locator("#column-a").dragTo(page.locator("#column-b"));
+  await page.waitForTimeout(5000);
+});
